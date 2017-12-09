6378a92fc260474c7b9eeef124d0a5e754610110
Ubuntu <ubuntu@ip-172-31-25-173.us-east-2.compute.internal>
2017-12-09 05:16:30 +0000

    fb verify

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+import config
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,3 +9,9 @@ def hello():
 
 if __name__ == "__main__":
 	app.run()
+
+@app.route("/surpriseme/")
+def verify():
+    if config.VERIFY_TOKEN == request.args.get('hub.verify_token'):
+        return request.args.get('hub.challenge')
+    return ''
