c0b94fe08e2896017acaf57eb049e8596579d3b2
Aishwarya Raj <araj6@illinois.edu>
2017-12-11 21:47:54 -0600

    update

--- datastore.py
+++ datastore.py
@@ -2,9 +2,10 @@ import pickledb
 
 db = pickledb.load('/var/www/SurpriseMe/realmp7data.db', False)
 try:
-    db.lgetall("users")
+    db.dgetall("users")
 except KeyError as e:
-    db.lcreate("users")
+    db.dcreate("users")
+    db.dump()
 
 # def passData(name, preferences, time, id):
 #     namepreftime = [name, preferences, time]
@@ -16,21 +17,21 @@ except KeyError as e:
 
 def getUsers():
     try:
-        return db.lgetall("users")
+        return db.dkeys("users")
     except KeyError as e:
-        db.lcreate("users")
+        db.dcreate("users")
         db.dump()
         return []
 
 def addUser(id, topics):
-    db.ladd("users",id)
+    db.dadd("users",(id, True))
     db.dcreate(id)
     db.dadd(id,("id",id))
     db.dadd(id,("topics",topics))
     db.dump()
 
 def setTime(id, time):
-    db.dadd(id, ("time", time))
+    db.dadd(id,("time", time))
     db.dump()
 
 def getUsersByTime(time):
