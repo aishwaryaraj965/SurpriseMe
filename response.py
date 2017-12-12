fdaae9a26e1e00ce0bb75a36bb3a744f50a712be
Aishwarya Raj <araj6@illinois.edu>
2017-12-11 21:59:27 -0600

    update

--- response.py
+++ response.py
@@ -8,18 +8,8 @@ time = datetime.datetime.now().hour
 
 users = datastore.getUsersByTime(time)
 
-
-for user in users:
-    try:
-        url = reddit.pick_sub(user["topics"])
-        response(user["id"], url)
-    except Exception as e:
-        print e.message
-
 #reply construction
 def response(senderId, articleUrl):
-    print senderId
-    print articleUrl
     articleData = {
         "recipient": {
             "id": senderId
@@ -30,3 +20,10 @@ def response(senderId, articleUrl):
 
     }
     requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + config.PAGE_ACCESS_TOKEN, json=articleData)
+
+for user in users:
+    try:
+        url = reddit.pick_sub(user["topics"])
+        response(user["id"], url)
+    except Exception as e:
+        print e.message
